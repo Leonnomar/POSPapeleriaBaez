0d4cb3d9e78550c34ceb49d9ebb050f328fb53c1
Ramón Báez <ramonnoelbaezlopez.15@gmail.com>
2026-05-29 21:10:27 -0700

    Iniciando el Punto de Venta de la Papeleria Baez

---
+++ PapeleriaBaez/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PapeleriaBaez.Models;
+
+namespace PapeleriaBaez.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Categoria> Categorias => Set<Categoria>();
+
+        public DbSet<Producto> Productos => Set<Producto>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=PapeleriaBaez.db");
+        }
+    }
+}
---
+++ PapeleriaBaez/Models/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PapeleriaBaez.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string Codigo { get; set; } = string.Empty;
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public decimal Costo { get; set; }
+
+        public decimal PrecioVenta { get; set; }
+
+        public int Stock { get; set; }
+
+        public int StockMinimo { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public Categoria? Categoria { get; set; }
+    }
+}
