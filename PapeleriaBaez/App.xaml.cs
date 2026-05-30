035cba72fd76f07f9ac5823dc4bf67b4bbaa9c33
Ramón Báez <ramonnoelbaezlopez.15@gmail.com>
2026-05-30 13:10:58 -0700

    Creando la BD y añadiendo las categorías

--- PapeleriaBaez/App.xaml.cs
+++ PapeleriaBaez/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using PapeleriaBaez.Data;
 
 namespace PapeleriaBaez
 {
@@ -9,6 +10,14 @@ namespace PapeleriaBaez
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            using var db = new AppDbContext();
+
+            db.Database.EnsureCreated();
+        }
     }
 
 }
