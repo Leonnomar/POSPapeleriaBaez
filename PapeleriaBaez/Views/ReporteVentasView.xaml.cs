2a39ad22a13814a66468823ae6c05334761c0b1c
Ramón Báez <ramonnoelbaezlopez.15@gmail.com>
2026-08-02 15:46:05 -0700

    Codificando la ventana de 'Reportes', buscando por fechas

--- PapeleriaBaez/Views/ReporteVentasView.xaml.cs
+++ PapeleriaBaez/Views/ReporteVentasView.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using PapeleriaBaez.Data;
+using PapeleriaBaez.Models;
 
 namespace PapeleriaBaez.Views
 {
@@ -20,14 +23,64 @@ namespace PapeleriaBaez.Views
     /// </summary>
     public partial class ReporteVentasView : UserControl
     {
+        private List<ReporteVentaGrid> listaVentas = new();
         public ReporteVentasView()
         {
             InitializeComponent();
+
+            dpDesde.SelectedDate = DateTime.Today;
+            dpHasta.SelectedDate = DateTime.Today;
+
+            CargarVentas();
+        }
+
+        private void CargarVentas()
+        {
+            using var db = new AppDbContext();
+
+            DateTime desde = dpDesde.SelectedDate?.Date ?? DateTime.Today;
+
+            DateTime hasta = (dpHasta.SelectedDate?.Date ?? DateTime.Today)
+                             .AddDays(1)
+                             .AddTicks(-1);
+
+            listaVentas = db.Ventas
+                .Include(v => v.Detalles)
+                .Where(v => v.Fecha >= desde &&
+                            v.Fecha <= hasta)
+                .OrderByDescending(v => v.Fecha)
+                .Select(v => new ReporteVentaGrid
+                {
+                    Id = v.Id,
+                    Fecha = v.Fecha,
+                    Productos = v.Detalles.Sum(d => d.Cantidad),
+                    Total = v.Total
+                })
+                .ToList();
+
+            dgVentas.ItemsSource = listaVentas;
+
+            ActualizarResumen();
+        }
+
+        private void ActualizarResumen()
+        {
+            int ventas = listaVentas.Count;
+
+            int productos = listaVentas.Sum(x => x.Productos);
+
+            decimal total = listaVentas.Sum(x => x.Total);
+
+            lblResumen.Text =
+                $"Ventas: {ventas}      " +
+                $"Artículos: {productos}        " +
+                $"Ingresos: {total:C}";
+
         }
 
         private void BtnBuscar_Click(object sender, RoutedEventArgs e)
         {
-
+            CargarVentas();
         }
 
         private void dgVentas_SelectionChanged(object sender, SelectionChangedEventArgs e)
